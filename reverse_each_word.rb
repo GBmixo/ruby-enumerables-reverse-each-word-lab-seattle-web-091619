4507f68e242fd47025b1a8fb26385efa07ba388f
Garrett Burns <garrettburns@mac.com>
2019-09-13 03:02:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   a = string.split(" ")
   b = a.collect {|buh| buh.split("")}
   reversed = []
-  reformed = reversed.join(" ")
+  reformed = reversed.collect {|words| words.join(" ")}
   count = 0
   
   reversing = b.collect { |word|
