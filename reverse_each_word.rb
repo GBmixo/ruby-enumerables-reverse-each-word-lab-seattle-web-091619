aea5aa3094585d3551ed34f75aa035ce20645f9d
Garrett Burns <garrettburns@mac.com>
2019-09-13 02:38:26 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+def reverse_each_word(string)
+  a = string.split(" ")
+  b = a.map {|buh| buh.split("")}
+  reversed = []
+  
+  reversing = b.map { |word|
+    #word.size.times { reversed << b.pop }
+    
+    moving = word.shift(0)
+    p moving
+    moved = reversed.push(moving)
+    #p moved
+    
+  }
+  p b
+end
+
+reverse_each_word("please reverse this")
+
+  #while count < array.length
+  #new.push(yield(array[count]))
+  #count += 1
+  #end
